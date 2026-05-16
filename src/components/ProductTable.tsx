1f6740e64d818895f1c269daf815a404c95f66fe
misaki <misaki.inoue01@gmail.com>
2026-05-16 14:18:16 +0900

    add: Item details rows component

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -4,10 +4,24 @@ type ItemHeaderRowProps = {
   itemName: ItemDetails["itemName"];
 };
 
+type ItemDetailsRowProps = {
+  price: ItemDetails["itemPrice"];
+  details: ItemDetails["itemCaption"];
+};
+
 function ItemHeaderRow({ itemName }: ItemHeaderRowProps) {
   return (
     <tr>
       <th>{itemName}</th>
+    </tr>
+  );
+}
+
+function ItemDetailRow({ price, details }: ItemDetailsRowProps) {
+  return (
+    <tr>
+      <td>{price}</td>
+      <td>{details}</td>
     </tr>
   );
 }
