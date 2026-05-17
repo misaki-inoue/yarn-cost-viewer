3666f61c9a21dbd1e4675b98222cf0af2f2ca0e0
misaki <misaki.inoue01@gmail.com>
2026-05-17 12:28:26 +0900

    add: Image components

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -1,4 +1,4 @@
-import type { RakutenApiResponse, ItemDetails } from "../types";
+import type { RakutenApiResponse, ItemDetails, RakutenImage } from "../types";
 
 type ItemHeaderRowProps = {
   name: ItemDetails["itemName"];
@@ -7,6 +7,14 @@ type ItemHeaderRowProps = {
 type ItemDetailsRowProps = {
   price: ItemDetails["itemPrice"];
   details: ItemDetails["itemCaption"];
+};
+
+type ImageListProps = {
+  images: ItemDetails["mediumImageUrls"];
+};
+
+type ItemImagesRowProps = {
+  itemImages: RakutenImage[];
 };
 
 function ItemHeaderRow({ name }: ItemHeaderRowProps) {
@@ -22,6 +30,30 @@ function ItemDetailRow({ price, details }: ItemDetailsRowProps) {
     <tr>
       <td>{price}</td>
       <td>{details}</td>
+    </tr>
+  );
+}
+
+function ItemImage({ imageUrl }: RakutenImage) {
+  return <img src={imageUrl} alt="" width={128} />;
+}
+
+function ImageList({ images }: ImageListProps) {
+  return (
+    <>
+      {images.map(({ imageUrl }) => (
+        <ItemImage key={imageUrl} imageUrl={imageUrl} />
+      ))}
+    </>
+  );
+}
+
+function ItemImagesRow({ itemImages }: ItemImagesRowProps) {
+  return (
+    <tr>
+      <td>
+        <ImageList images={itemImages} />
+      </td>
     </tr>
   );
 }
